b543df6247b8fc9ceaf229dacb84d1433997689c
Wendy Arevalo <wendyyunuen@hotmail.com>
2023-02-06 16:25:08 +0200

    Part3.12 console mongo.js

---
+++ part3/phonebook/phonebook-backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://fullstack-wyae:${password}@cluster0.b4j42te.mongodb.net/personsApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv[3]){
+    const personName = process.argv[3]
+    const personNumber = process.argv[4]
+
+    const person = new Person({
+        name: personName,
+        number: personNumber,
+    })
+
+    person.save().then(result => {
+        console.log('added ',personName,' number ', personNumber, ' to phonebook')
+        mongoose.connection.close()
+    })
+}else{
+    Person.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(person => {
+            console.log(person.name, " ", person.number)
+        })
+        mongoose.connection.close()
+    })
+}
