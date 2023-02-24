f3c0c910a7e7bad0516ece77ccd06cb4e0f1b9da
Wendy Arevalo <wendyyunuen@hotmail.com>
2023-02-24 12:38:04 +0200

    Part4.5-7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodashTools = require('lodash');
+
 const totalLikes = (blogs) => {
 
     const initialValue = 0;
@@ -7,11 +9,35 @@ const totalLikes = (blogs) => {
     )
 }
 
+const favoriteBlog = (blogs) => {
+    blogs.sort((a, b) => b["likes"] - a["likes"]);
+    return blogs[0];
+}
+
+const mostBlogs = (blogs) => {
+    const totalByAuthor = lodashTools.countBy(blogs, 'author')
+    const entries = Object.entries(totalByAuthor);
+    entries.sort((a, b) => b[1] - a[1]);
+    return( blogs.length > 0 ? { 'author': entries[0][0], 'blogs': entries[0][1] } : {})
+}
+
+const mostLikes = (blogs) => {
+    const blogsByAuthor = lodashTools.groupBy(blogs, 'author')
+    const blogsByLikes = lodashTools.mapValues(blogsByAuthor, totalLikes)
+    const entries = Object.entries(blogsByLikes);
+    entries.sort((a, b) => b[1] - a[1]);
+    return( blogs.length > 0 ? { 'author': entries[0][0], 'likes': entries[0][1] } : {})
+}
+
+
 const dummy = (blogs) => {
     return 1
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
