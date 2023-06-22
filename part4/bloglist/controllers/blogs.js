e5bbc11940ec0afeaef1cb2c700d5ef197e22795
Wendy Arevalo <wendyyunuen@hotmail.com>
2023-06-22 21:01:51 +0300

    Part4.13-14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', async(request, response) => {
+blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     response.json(blogs)
 })
@@ -19,6 +19,24 @@ blogRouter.post('/', async (request, response, next) => {
         const savedBlog = await blog.save()
         response.status(201).json(savedBlog)
     } catch(exception) {
+        next(exception)
+    }
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
+    const blog = {
+        ...request.body
+    }
+
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+        response.json(updatedBlog)
+    } catch (exception) {
         next(exception)
     }
 })
--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -17,18 +17,20 @@ beforeEach(async () => {
     await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
+describe('when there is initially some blogs saved', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
 
 
-test('there are two blogs', async () => {
-    const response = await api.get('/api/blogs')
+    test('there are two blogs', async () => {
+        const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
 })
 
 test('verify id exists', async () => {
@@ -90,6 +92,44 @@ describe('missing property', () => {
         expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
     })
 })
+
+describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        expect(blogsAtEnd).toHaveLength(
+            helper.initialBlogs.length - 1
+        )
+
+        const titles = blogsAtEnd.map(b => b.title)
+
+        expect(titles).not.toContain(blogToDelete.title)
+    })
+})
+
+test('successfully updates a blog', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    console.log(blogToUpdate)
+    const modifiedBlog = {
+        ...blogToUpdate,
+        likes: blogToUpdate.likes + 1
+    }
+    console.log(modifiedBlog)
+    const response = await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(modifiedBlog)
+
+    expect(response.body.likes).toBe(blogToUpdate.likes + 1)
+})
+
 
 afterAll(async () => {
     await mongoose.connection.close()
