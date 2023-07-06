9534d97ef4dc7cea7059b0689f730a1de0cf9b0e
Wendy Arevalo <wendyyunuen@hotmail.com>
2023-07-06 19:59:19 +0300

    Part5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = {display: visible ? 'none' : ''}
+  const showWhenVisible = {display: visible ? '' : 'none'}
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+      <div style={blogStyle}>
+        <div style={hideWhenVisible}>
+          {blog.title} {blog.author}
+          <button onClick={toggleVisibility}>view</button>
+        </div>
+        <div style={showWhenVisible}>
+          {blog.title} {blog.author}
+          <button onClick={toggleVisibility}>hide</button>
+          <div>{blog.url}</div>
+          <div>{blog.likes} likes <button>like</button></div>
+          <div>{blog.user.name}</div>
+        </div>
+      </div>
+  )
+}
 
 export default Blog
